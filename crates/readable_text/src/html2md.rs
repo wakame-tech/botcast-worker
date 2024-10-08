cbfd68176b050dd21914b0352f4724173dd93df6
wakame-tech <kamata1919@gmail.com>
2024-10-09 01:32:45 +0900

    fix: ignore head, style tag

--- crates/readable_text/src/html2md.rs
+++ crates/readable_text/src/html2md.rs
@@ -7,6 +7,7 @@ pub struct Html2MdExtractor;
 impl Extractor for Html2MdExtractor {
     fn extract(html: &str) -> anyhow::Result<String> {
         let mut handlers = HashMap::<String, Box<dyn TagHandlerFactory>>::new();
+        handlers.insert("head".to_string(), Box::new(IgnoreHandlerFactory));
         handlers.insert("h1".to_string(), Box::new(HeaderHandlerFactory));
         handlers.insert("h2".to_string(), Box::new(HeaderHandlerFactory));
         handlers.insert("h3".to_string(), Box::new(HeaderHandlerFactory));
@@ -19,6 +20,7 @@ impl Extractor for Html2MdExtractor {
         handlers.insert("nav".to_string(), Box::new(DummyHandlerFactory));
         handlers.insert("pre".to_string(), Box::new(IgnoreHandlerFactory));
         handlers.insert("script".to_string(), Box::new(IgnoreHandlerFactory));
+        handlers.insert("style".to_string(), Box::new(IgnoreHandlerFactory));
         let md = html2md::parse_html_custom(html, &handlers);
         Ok(md)
     }
--- crates/worker/src/bin/cli.rs
+++ crates/worker/src/bin/cli.rs
@@ -1,5 +1,7 @@
 use clap::Parser;
 use readable_text::{html2md::Html2MdExtractor, Extractor};
+use std::fs::OpenOptions;
+use std::io::Write;
 use worker::infra::http_client::HttpClient;
 
 #[derive(Debug, clap::Parser)]
@@ -12,8 +14,18 @@ async fn main() -> anyhow::Result<()> {
     let client = HttpClient::default();
     let args = Args::try_parse()?;
     let html = client.fetch_content_as_utf8(args.url.parse()?).await?;
-    println!("{}", html);
+    let mut out_html = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open("out.html")?;
+    writeln!(out_html, "{}", html)?;
     let md = Html2MdExtractor::extract(&html)?;
-    println!("{}", md);
+    let mut out_md = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open("out.md")?;
+    writeln!(out_md, "{}", md)?;
     Ok(())
 }
