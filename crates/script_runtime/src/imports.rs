033fab974afe4eaf7e17c9e6830c0c34c73f46c9
wakame-tech <kamata1919@gmail.com>
2024-10-12 12:58:33 +0900

    fix: imports make async

--- crates/script_runtime/src/imports.rs
+++ crates/script_runtime/src/imports.rs
@@ -1,5 +1,6 @@
 use crate::Urn;
 use anyhow::Result;
+use futures::{future::BoxFuture, FutureExt};
 use json_e::{
     value::{Function, Value},
     Context,
@@ -8,56 +9,68 @@ use json_e::{
 pub(crate) struct Imports;
 
 impl Imports {
-    pub(crate) fn today(&self, format: String) -> String {
+    pub(crate) async fn today(&self, format: String) -> String {
         chrono::Local::now().format(&format).to_string()
     }
 
-    pub(crate) fn get(&self, urn: Urn) -> String {
+    pub(crate) async fn get(&self, urn: Urn) -> String {
         // TODO
         urn.0
     }
 
-    pub(crate) fn fetch(&self, url: String) -> String {
+    pub(crate) async fn fetch(&self, url: String) -> String {
         // TODO
         url
     }
 
-    pub(crate) fn llm(&self, _prompt: String) -> String {
+    pub(crate) async fn llm(&self, _prompt: String) -> String {
         // TODO
         "llm".to_string()
     }
 }
 
-fn today(_: &Context<'_>, args: &[Value]) -> Result<Value> {
-    let imports = Imports;
-    match args {
-        [Value::String(format)] => Ok(Value::String(imports.today(format.clone()))),
-        _ => Err(anyhow::anyhow!("today only supports a string".to_string())),
+fn today<'a>(_: &Context<'_>, args: &'a [Value]) -> BoxFuture<'a, Result<Value>> {
+    async move {
+        let imports = Imports;
+        match args {
+            [Value::String(format)] => Ok(Value::String(imports.today(format.clone()).await)),
+            _ => Err(anyhow::anyhow!("today only supports a string".to_string())),
+        }
     }
+    .boxed()
 }
 
-fn get(_: &Context<'_>, args: &[Value]) -> Result<Value> {
-    let imports = Imports;
-    match args {
-        [Value::String(urn)] => Ok(Value::String(imports.get(Urn(urn.clone())))),
-        _ => Err(anyhow::anyhow!("get only supports a string".to_string())),
+fn get<'a>(_: &Context<'_>, args: &'a [Value]) -> BoxFuture<'a, Result<Value>> {
+    async move {
+        let imports = Imports;
+        match args {
+            [Value::String(urn)] => Ok(Value::String(imports.get(Urn(urn.clone())).await)),
+            _ => Err(anyhow::anyhow!("get only supports a string".to_string())),
+        }
     }
+    .boxed()
 }
 
-fn fetch(_: &Context<'_>, args: &[Value]) -> Result<Value> {
-    let imports = Imports;
-    match args {
-        [Value::String(url)] => Ok(Value::String(imports.fetch(url.clone()))),
-        _ => Err(anyhow::anyhow!("fetch only supports a string".to_string())),
+fn fetch<'a>(_: &Context<'_>, args: &'a [Value]) -> BoxFuture<'a, Result<Value>> {
+    async move {
+        let imports = Imports;
+        match args {
+            [Value::String(url)] => Ok(Value::String(imports.fetch(url.clone()).await)),
+            _ => Err(anyhow::anyhow!("fetch only supports a string".to_string())),
+        }
     }
+    .boxed()
 }
 
-fn llm(_: &Context<'_>, args: &[Value]) -> Result<Value> {
-    let imports = Imports;
-    match args {
-        [Value::String(prompt)] => Ok(Value::String(imports.llm(prompt.clone()))),
-        _ => Err(anyhow::anyhow!("llm only supports a string".to_string())),
+fn llm<'a>(_: &Context<'_>, args: &'a [Value]) -> BoxFuture<'a, Result<Value>> {
+    async move {
+        let imports = Imports;
+        match args {
+            [Value::String(prompt)] => Ok(Value::String(imports.llm(prompt.clone()).await)),
+            _ => Err(anyhow::anyhow!("llm only supports a string".to_string())),
+        }
     }
+    .boxed()
 }
 
 pub(crate) fn define_imports<'a>(context: &mut Context<'a>) {
--- crates/script_runtime/src/runtime.rs
+++ crates/script_runtime/src/runtime.rs
@@ -2,48 +2,55 @@ use crate::{imports::define_imports, Manuscript};
 use anyhow::Result;
 use json_e::Context;
 
-pub fn run(template: &serde_json::Value) -> Result<Manuscript> {
-    let mut context = Context::default();
+pub async fn run(template: &serde_json::Value) -> Result<Manuscript> {
+    let mut context = Context::new();
     define_imports(&mut context);
-    let result = json_e::render(template, &context)?;
+    let result = json_e::render_with_context(template, &context).await?;
     let manuscript: Manuscript = serde_json::from_value(result)?;
     Ok(manuscript)
 }
 
 #[cfg(test)]
 mod tests {
+    use std::time::Duration;
+
     use anyhow::Result;
+    use futures::{future::BoxFuture, FutureExt};
     use json_e::{
         value::{Function, Value},
         Context,
     };
     use serde_json::json;
 
-    fn add(_: &Context<'_>, args: &[Value]) -> Result<Value> {
-        match (&args[0], &args[1]) {
-            (Value::Number(a), Value::Number(b)) => Ok(Value::Number(a + b)),
-            _ => Err(anyhow::anyhow!("add only supports numbers, got {:?}", args)),
+    fn add<'a>(_: &Context<'_>, args: &'a [Value]) -> BoxFuture<'a, Result<Value>> {
+        async move {
+            tokio::time::sleep(Duration::from_secs(3)).await;
+            match (&args[0], &args[1]) {
+                (Value::Number(a), Value::Number(b)) => Ok(Value::Number(a + b)),
+                _ => Err(anyhow::anyhow!("add only supports numbers, got {:?}", args)),
+            }
         }
+        .boxed()
     }
 
     fn custom_context<'a>() -> Context<'a> {
-        let mut context = Context::default();
+        let mut context = Context::new();
         context.insert("today", Value::String("xx月yy日".to_string()));
         context.insert("add", Value::Function(Function::new("add", add)));
         context
     }
 
-    #[test]
-    fn test_custom_function() -> Result<()> {
+    #[tokio::test]
+    async fn test_custom_function() -> Result<()> {
         let template = json!("こんにちは、${today} 1+2=${add(1, 2)}");
         let context = custom_context();
-        let result = json_e::render(&template, &context)?;
+        let result = json_e::render_with_context(&template, &context).await?;
         assert_eq!(result, json!("こんにちは、xx月yy日 1+2=3"));
         Ok(())
     }
 
-    #[test]
-    fn test_nested_let() -> Result<()> {
+    #[tokio::test]
+    async fn test_nested_let() -> Result<()> {
         let template = json!({
             "$let": {
                 "a": { "$eval": "add(1, 2)" },
@@ -61,7 +68,7 @@ mod tests {
             }
         });
         let context = custom_context();
-        let result = json_e::render(&template, &context)?;
+        let result = json_e::render_with_context(&template, &context).await?;
         assert_eq!(
             result,
             json!(serde_json::to_string(&json!({"b": "hoge", "c": "3"})).unwrap()),
