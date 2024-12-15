1658bf6afa106dbe55fe1ec8b98610a28227fcda
wakame-tech <kamata1919@gmail.com>
2024-12-15 13:07:19 +0900

    fix(script): readable_text

--- crates/readable_text/src/html2md.rs
+++ crates/readable_text/src/html2md.rs
@@ -5,25 +5,33 @@ pub struct ReadableText;
 
 impl ReadableText {
     pub fn extract(html: &str) -> anyhow::Result<String> {
-        let mut handlers = HashMap::<String, Box<dyn TagHandlerFactory>>::new();
-        handlers.insert("head".to_string(), Box::new(IgnoreHandlerFactory));
-        handlers.insert("h1".to_string(), Box::new(HeaderHandlerFactory));
-        handlers.insert("h2".to_string(), Box::new(HeaderHandlerFactory));
-        handlers.insert("h3".to_string(), Box::new(HeaderHandlerFactory));
-        handlers.insert("h4".to_string(), Box::new(HeaderHandlerFactory));
-        handlers.insert("h5".to_string(), Box::new(HeaderHandlerFactory));
-        handlers.insert("h6".to_string(), Box::new(HeaderHandlerFactory));
-        handlers.insert("a".to_string(), Box::new(DummyHandlerFactory));
-        handlers.insert("img".to_string(), Box::new(DummyHandlerFactory));
-        handlers.insert("table".to_string(), Box::new(DummyHandlerFactory));
-        handlers.insert("nav".to_string(), Box::new(DummyHandlerFactory));
-        handlers.insert("pre".to_string(), Box::new(IgnoreHandlerFactory));
-        handlers.insert("script".to_string(), Box::new(IgnoreHandlerFactory));
-        handlers.insert("style".to_string(), Box::new(IgnoreHandlerFactory));
-        handlers.insert("iframe".to_string(), Box::new(IgnoreHandlerFactory));
-        let md = html2md::parse_html_custom(html, &handlers);
+        let md = parse_to_md(html);
+        let md = md.replace("\n", " ");
         Ok(md)
     }
+}
+
+fn parse_to_md(html: &str) -> String {
+    let mut handlers = HashMap::<String, Box<dyn TagHandlerFactory>>::new();
+    handlers.insert("head".to_string(), Box::new(IgnoreHandlerFactory));
+    handlers.insert("h1".to_string(), Box::new(HeaderHandlerFactory));
+    handlers.insert("h2".to_string(), Box::new(HeaderHandlerFactory));
+    handlers.insert("h3".to_string(), Box::new(HeaderHandlerFactory));
+    handlers.insert("h4".to_string(), Box::new(HeaderHandlerFactory));
+    handlers.insert("h5".to_string(), Box::new(HeaderHandlerFactory));
+    handlers.insert("h6".to_string(), Box::new(HeaderHandlerFactory));
+    handlers.insert("a".to_string(), Box::new(DummyHandlerFactory));
+    handlers.insert("em".to_string(), Box::new(DummyHandlerFactory));
+    handlers.insert("strong".to_string(), Box::new(DummyHandlerFactory));
+    handlers.insert("img".to_string(), Box::new(IgnoreHandlerFactory));
+    handlers.insert("table".to_string(), Box::new(DummyHandlerFactory));
+    handlers.insert("nav".to_string(), Box::new(DummyHandlerFactory));
+    handlers.insert("pre".to_string(), Box::new(IgnoreHandlerFactory));
+    handlers.insert("script".to_string(), Box::new(IgnoreHandlerFactory));
+    handlers.insert("style".to_string(), Box::new(IgnoreHandlerFactory));
+    handlers.insert("iframe".to_string(), Box::new(IgnoreHandlerFactory));
+    let md = html2md::parse_html_custom(html, &handlers);
+    md
 }
 
 struct IgnoreHandlerFactory;
@@ -73,5 +81,23 @@ struct HeaderHandlerFactory;
 impl TagHandlerFactory for HeaderHandlerFactory {
     fn instantiate(&self) -> Box<dyn TagHandler> {
         Box::new(HeaderHandler)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_extract() {
+        for (html, expected_md) in [
+            (r#"<em>hoge</em>"#, "hoge"),
+            (r#"<strong>hoge</strong>"#, "hoge"),
+            (r#"<img src="a"></img>"#, ""),
+            (r#"<a href="http://example.com">hoge</a>"#, "hoge"),
+        ] {
+            let md = ReadableText::extract(html).unwrap();
+            assert_eq!(md, expected_md);
+        }
     }
 }
